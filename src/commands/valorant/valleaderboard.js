31b8e803a5c83a600b2278de28df45e7c374f898
Miguel Ângelo <miguelssantos96@gmail.com>
2023-05-02 09:51:36 +0000

    added scheduler

--- src/commands/valorant/valleaderboard.js
+++ src/commands/valorant/valleaderboard.js
@@ -2,54 +2,49 @@ const {
     Client,
     Interaction
   } = require('discord.js');
-
-const { AllPlayersLeaderBoardEmbed } = require('../../utils/Embeds/ValLeaderBoardEmbed');
-
-
-module.exports = {
-    /**
-    * 
-    * @param {Client} client 
-    * @param {Interaction} interaction
-    * 
-    * @returns
-    * */
-
-    callback : async (client, interaction) => {
-        let guild, channel;
-
-        try{
-            if (!interaction.inGuild()) {
-                interaction.reply('You can only run this command inside a server.');
-                return;
-            }
-
-            await interaction.deferReply();
-
-            // Get the guild and the channel to send the message
-            guild = await client.guilds.fetch(interaction.guildId); 
-            channel = guild.channels.cache.find(channel => channel.name === 'newb-ranking');
-
-            // Validate if the command was executed in the correct channel
-            if(interaction.channelId !== channel.id){
-                interaction.editReply(`You can only run this command inside the ${channel} channel`);
-                return;
-            }
-
-            const embed = await AllPlayersLeaderBoardEmbed(guild);
-
-            //Delete all messages in the channel if exists
-            const messages = await channel.messages.fetch();
-            messages.forEach(message => message.delete());
-
-            //Send the embed
-            channel.send({ embeds: [embed] });
-
-        }catch(error){
-            console.log(error);
+  
+  const { AllPlayersLeaderBoardEmbed } = require('../../utils/Embeds/ValLeaderBoardEmbed');
+  
+  const UpdateLeaderboard = async (client, interaction = null) => {
+    let guild, channel;
+  
+    try {
+      if (interaction) {
+        if (!interaction.inGuild()) {
+          interaction.reply('You can only run this command inside a server.');
+          return;
         }
-
-    },
+  
+        await interaction.deferReply();
+      }
+  
+      // Get the guild and the channel to send the message
+      guild = await client.guilds.fetch(interaction.guildId);
+      channel = guild.channels.cache.find(channel => channel.name === 'newb-ranking');
+  
+      // Validate if the command was executed in the correct channel
+      if (interaction && interaction.channelId !== channel.id) {
+        interaction.editReply(`You can only run this command inside the ${channel} channel`);
+        return;
+      }
+  
+      const embed = await AllPlayersLeaderBoardEmbed(guild);
+  
+      // Delete all messages in the channel if exists
+      const messages = await channel.messages.fetch();
+      messages.forEach(message => message.delete());
+  
+      // Send the embed
+      channel.send({ embeds: [embed] });
+  
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  module.exports = {
+    callback: UpdateLeaderboard,
     name: 'valleaderboard',
     description: 'Get the Updated Valorant leaderboard of all registered players',
-}
+  };
+  
