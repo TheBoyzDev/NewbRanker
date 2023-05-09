2bd5249df753384f2c7021582a88c6bcb6f1dca0
Muffin <alexandru.lib@hotmail.com>
2023-05-09 21:03:38 +0100

    Leaderboard Update

--- src/utils/Embeds/ValLeaderBoardEmbed.js
+++ src/utils/Embeds/ValLeaderBoardEmbed.js
@@ -13,7 +13,10 @@ async function AllPlayersLeaderBoardEmbed(guild){
             .setTitle('**Valorant Leaderboard**')
             .setColor('#0099ff')
             .setThumbnail('https://i.imgflip.com/708n2y.gif')
-            //.setThumbnail('https://i.pinimg.com/originals/80/8a/c8/808ac89e0d67b242d0a6b98b1ebfdc38.gif')
+            .setFooter({ 
+                text: 'Made with love by Papi Neurotic and 5% Muffin', 
+                iconURL: 'https://www.icegif.com/wp-content/uploads/heart-icegif-38.gif' 
+            });
 
             players.forEach((player, index) => {
             // Get the corresponding emoji for the player's rank
@@ -21,14 +24,12 @@ async function AllPlayersLeaderBoardEmbed(guild){
             const rankEmoji = guild.emojis.cache.find(emoji => emoji.name === rankEmojiName);
             const link = `https://tracker.gg/valorant/profile/riot/${[player.val_name.replace(/\s+/g, '')]}%23${player.val_tag}/overview`;
 
-            playersArray.push(`\n\**\`\`#${index + 1}\`\`\** [${player.discordName.split('#')[0]}](${link}) 
-                                **ELO:** ${player.val_elo} | **Rank:** ${player.val_playerRank} ${rankEmoji}\n`);
+            playersArray.push(`\n\**\`\`#${index + 1}\`\`\** [${player.discordName.split('#')[0]}](${link})\n**ELO:** ${player.val_elo} | **Rank:** ${player.val_playerRank} ${rankEmoji}\n`);
             });
 
             // Add the player's info with the rank emoji to the embed in a single line
             let resultArray = playersArray.join(' ');
             leaderboardEmbed.setDescription(resultArray);
-            
 
     return leaderboardEmbed;
 };
