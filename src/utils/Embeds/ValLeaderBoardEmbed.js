7de42246ef4e840a3b126f51a10d038a5fa91e13
Muffin <alexandru.lib@hotmail.com>
2023-05-09 20:16:09 +0100

    Leaderboard Update

--- src/utils/Embeds/ValLeaderBoardEmbed.js
+++ src/utils/Embeds/ValLeaderBoardEmbed.js
@@ -10,7 +10,7 @@ async function AllPlayersLeaderBoardEmbed(guild){
 
     const leaderboardEmbed = new EmbedBuilder()
             //.setType('rich')
-            .setTitle('Valorant Leaderboard')
+            .setTitle('**Valorant Leaderboard**')
             .setColor('#0099ff')
             .setThumbnail('https://i.imgflip.com/708n2y.gif')
             //.setThumbnail('https://i.pinimg.com/originals/80/8a/c8/808ac89e0d67b242d0a6b98b1ebfdc38.gif')
@@ -21,8 +21,8 @@ async function AllPlayersLeaderBoardEmbed(guild){
             const rankEmoji = guild.emojis.cache.find(emoji => emoji.name === rankEmojiName);
             const link = `https://tracker.gg/valorant/profile/riot/${[player.val_name.replace(/\s+/g, '')]}%23${player.val_tag}/overview`;
 
-            playersArray.push(`\n\`\`#${index + 1}\`\` [${player.discordName.split('#')[0]}](${link}) 
-                                ELO: ${player.val_elo} | Rank: ${player.val_playerRank} ${rankEmoji}\n`);
+            playersArray.push(`\n\**\`\`#${index + 1}\`\`\** [${player.discordName.split('#')[0]}](${link}) 
+                                **ELO:** ${player.val_elo} | **Rank:** ${player.val_playerRank} ${rankEmoji}\n`);
             });
 
             // Add the player's info with the rank emoji to the embed in a single line
