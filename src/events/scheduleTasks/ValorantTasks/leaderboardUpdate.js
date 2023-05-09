8c24cab41e119148da6e138e1dba68e8b4d08cfa
Muffin <alexandru.lib@hotmail.com>
2023-05-09 19:55:06 +0100

    Leaderboard Update

--- src/events/scheduleTasks/ValorantTasks/leaderboardUpdate.js
+++ src/events/scheduleTasks/ValorantTasks/leaderboardUpdate.js
@@ -4,7 +4,7 @@ const { callback: UpdateLeaderboard } = require('../../../commands/valorant/vall
 
 module.exports = async (client) => {
     // Schedule a job to run every Hour
-    cron.schedule('0 * * * *', () => {
+    cron.schedule('* * * * *', () => {
         console.log('Updating leaderboard...');    
 
         UpdateLeaderboard(client).catch((error) => {
--- src/utils/Embeds/ValLeaderBoardEmbed.js
+++ src/utils/Embeds/ValLeaderBoardEmbed.js
@@ -5,12 +5,15 @@ const ValorantPlayer = require('../../models/ValorantPlayer');
 
 async function AllPlayersLeaderBoardEmbed(guild){
 
+    let playersArray = [];
     const players = await ValorantPlayer.find().sort({ val_elo: -1 });
 
     const leaderboardEmbed = new EmbedBuilder()
-            .setColor('#0099ff')
+            //.setType('rich')
             .setTitle('Valorant Leaderboard')
-            .setTimestamp();
+            .setColor('#0099ff')
+            .setThumbnail('https://i.imgflip.com/708n2y.gif')
+            //.setThumbnail('https://i.pinimg.com/originals/80/8a/c8/808ac89e0d67b242d0a6b98b1ebfdc38.gif')
 
             players.forEach((player, index) => {
             // Get the corresponding emoji for the player's rank
@@ -18,13 +21,14 @@ async function AllPlayersLeaderBoardEmbed(guild){
             const rankEmoji = guild.emojis.cache.find(emoji => emoji.name === rankEmojiName);
             const link = `https://tracker.gg/valorant/profile/riot/${[player.val_name.replace(/\s+/g, '')]}%23${player.val_tag}/overview`;
 
+            playersArray.push(`\n'``#${index + 1}``' [${player.discordName.split('#')[0]}](${link}) 
+            ELO: ${player.val_elo} | Rank: ${player.val_playerRank} ${rankEmoji}\n`);
+            });
+
             // Add the player's info with the rank emoji to the embed in a single line
-            leaderboardEmbed.addFields(
-                { name: `#${index + 1} ${player.discordName.split('#')[0]}`, 
-                value: `ELO: ${player.val_elo} | Rank: ${player.val_playerRank} ${rankEmoji}
-                [Tracker](${link})` , inline: false }
-            );
-        });
+            let resultArray = playersArray.join(' ');
+            leaderboardEmbed.setDescription(resultArray);
+            
 
     return leaderboardEmbed;
 };
